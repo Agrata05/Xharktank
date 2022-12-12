a860e6662244f185b428f79320e35798d70e690a
Agrata Tiwari <62853703+Agrata05@users.noreply.github.com>
2022-12-12 23:18:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require("mongoose")
 const cors = require('cors')
-const process.env.PORT || 8081
+const PORT = process.env.PORT || 8081
 const routes = require("./routes/routes.js");
 
 // To remove the CORS error
