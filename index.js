d463111058171d422706c06208f1116e6f9486b8
Agrata Tiwari <62853703+Agrata05@users.noreply.github.com>
2022-12-13 00:08:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const cors = require('cors')
 const PORT = process.env.PORT || 8081
 const routes = require("./Routes/routes.js");
-
+const DB_URI = process.env.DB_URI
 // To remove the CORS error
 app.use(cors())
 // To parse the body data into json 
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(routes);
 
-mongoose.connect('mongodb://localhost:27017/xharktank').then(() => {
+mongoose.connect(DB_URI).then(() => {
     app.listen(PORT, function(err){
         if (err) console.log("Error in server setup")
         console.log("Server listening on Port", PORT);
